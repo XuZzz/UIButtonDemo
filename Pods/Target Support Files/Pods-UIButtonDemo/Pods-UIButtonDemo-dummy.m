a9e52a6e8733b4d5d3d10f6fe3b8fc694c23519e
Xu <wowxznianshao@163.com>
2018-06-01 15:59:34 +0800

    XZbutton
    UIbutton自定义，可以实现四种image&text 的组合。
    XZButtonTypeNormal, // 图左文右
    XZButtonTypePicRight,// 图右文左
    XZButtonTypePicTop,// 图上文下
    XZButtonTypePicBottom,// 图下文上

---
+++ Pods/Target Support Files/Pods-UIButtonDemo/Pods-UIButtonDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIButtonDemo : NSObject
+@end
+@implementation PodsDummy_Pods_UIButtonDemo
+@end
--- UIButtonDemo/ViewController.m
+++ UIButtonDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+#import "XZButton.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    XZButton *button = [[XZButton alloc] initWithFrame:CGRectMake(100, 100, 30, 100) withType:XZButtonTypePicBottom andMargin:10];
+    
+    XZButton *button = [[XZButton alloc] init];
+    
+    button.type = XZButtonTypePicRight;
+    
+    button.textMargin = 10;
+    
+    [self.view addSubview:button];
+    
+    [button setTitle:@"hahaha" forState:UIControlStateNormal];
+    
+    [button setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    
+    button.titleLabel.font = [UIFont systemFontOfSize:14.0f weight:UIFontWeightThin];
+    
+    button.imageView.image = [UIImage imageNamed:@"sina_gray.png"];
+    
+    [button mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.center.equalTo(self.view);
+        
+        make.size.mas_equalTo(CGSizeMake(100, 30));
+    }];
+    
+    
 }
 
 
